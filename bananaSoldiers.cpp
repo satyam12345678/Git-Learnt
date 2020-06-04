a99a01ba094fa78c470cdc0525c959d5c5849373
Satyam Tripathi <sk729485@gmail.com>
2020-06-04 21:00:54 +0530

    Initial Commit

---
+++ bananaSoldiers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+	long long int k,n,w;
+	cin>>k>>n>>w;
+	long long int totalC=0;
+	for(long long int i=w;i>0;i--)
+	{
+		totalC+=(i*k);
+	}
+	if(totalC>n)
+	cout<<totalC-n;
+	else
+	cout<<"0";
+} 
